7ea0a5f80be4df8c0ae9da337eb1d2adef4fa425
Omar Sharif <omarsharif.swe@gmail.com>
2025-07-23 16:07:43 +0600

    update of learning of webdriver common by and import BY and x path

--- tests/newPython.py
+++ tests/newPython.py
@@ -1,6 +1,12 @@
+from time import sleep
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+
 
 
 driver = webdriver.Chrome()
-driver.get('https://selenium.dev/')
-driver.quit()
+driver.get('https://mental-health-check-nine.vercel.app/')
+sleep(10)
+button = driver.find_element(By.XPATH,"//button[@class='justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2 flex items-center gap-2']")
+button.click()
+sleep(10)
