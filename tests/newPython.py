5e9fd669f388b6de8ec23af92ec4f8ade6073b3a
Omar Sharif <omarsharif.swe@gmail.com>
2025-07-23 17:07:43 +0600

    logging successfully

--- tests/newPython.py
+++ tests/newPython.py
@@ -10,3 +10,8 @@ sleep(10)
 button = driver.find_element(By.XPATH,"//button[@class='justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2 flex items-center gap-2']")
 button.click()
 sleep(10)
+
+driver.find_element(By.ID,"email").send_keys("irontester80@gmail.com")
+driver.find_element(By.ID,"password").send_keys("12345678")
+driver.find_element(By.XPATH,"//button[@type='submit']").click()
+sleep(10)
